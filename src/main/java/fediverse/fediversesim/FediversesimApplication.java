664ff17368274b7b16323d9474e1006d88ff439f
blueberry <jnb@campus.uni-paderborn.de>
2024-11-17 16:53:02 +0100

    feat: inital commit

---
+++ src/main/java/fediverse/fediversesim/FediversesimApplication.java
@@ -0,0 +1,13 @@
+package fediverse.fediversesim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FediversesimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FediversesimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fediverse/fediversesim/FediversesimApplicationTests.java
@@ -0,0 +1,13 @@
+package fediverse.fediversesim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FediversesimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

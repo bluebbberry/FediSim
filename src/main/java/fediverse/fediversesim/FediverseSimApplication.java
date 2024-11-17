748254c22ed53ff87f6ae2fdf9cd45004253999f
blueberry <jnb@campus.uni-paderborn.de>
2024-11-17 16:58:32 +0100

    feat: add some basic classes

--- src/main/java/fediverse/fediversesim/FediverseSimApplication.java
+++ src/main/java/fediverse/fediversesim/FediverseSimApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FediversesimApplication {
+public class FediverseSimApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FediversesimApplication.class, args);
+		SpringApplication.run(FediverseSimApplication.class, args);
 	}
 
 }
